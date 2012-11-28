5db208149a2e03b949b6196804f26e8e6af54d5b
Nick Nystrom <nick.nystrom@belaysoftware.com>
2012-11-28 15:00:22 -0600

    Initial checkin.

---
+++ Mavo.Asset/Models/AssetModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Mavo.Asset.Models
+{
+    public enum AssetKind
+    {
+        Consumable,
+        Durable,
+        DurableNotTracked,
+    }
+
+    public class AssetClass
+    {
+
+    }
+}
---
+++ Mavo.Asset/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mavo.Asset")]
+[assembly: AssemblyDescription("Mavo Asset Tracking System")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mavo.Asset")]
+[assembly: AssemblyCopyright("Copyright © Mavo, Inc & Apex Online Development, Inc. 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("697fee09-4b91-4172-98c0-d1ad364023ec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
