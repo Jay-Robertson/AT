306e889caae572147cc8f0d2354001f71b6befde
Scott <scott@redbranchsoftware.com>
2012-12-20 14:42:26 -0600

    Changes to what we show in the menu Added a coloring for Being picked

--- Mavo.Asset/Controllers/BaseController.cs
+++ Mavo.Asset/Controllers/BaseController.cs
@@ -12,11 +12,10 @@ namespace Mavo.Assets.Controllers
     {
         protected override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-
             AssetContext assetContext =  ServiceLocator.Current.GetInstance<AssetContext>();
             ViewBag.Templates = assetContext.Templates.ToList();
-            ViewBag.JobsReadyToPickForNav = assetContext.Jobs.Where(x => x.Status == JobStatus.ReadyToPick).ToList().GroupBy(x => x.PickupTime.Date).OrderBy(x => x.Key);
-            ViewBag.JobsReadyForReturn = assetContext.Jobs.Where(x => x.Status == JobStatus.Started && x.PickCompleted.HasValue).ToList().GroupBy(x => x.PickCompleted.Value.Date).OrderBy(x => x.Key);
+            ViewBag.JobsReadyToPickForNav = assetContext.Jobs.Where(x => x.Status == JobStatus.ReadyToPick || x.Status == JobStatus.BeingPicked).ToList().GroupBy(x => x.PickupTime.Date).OrderBy(x => x.Key);
+            ViewBag.JobsReadyForReturn = assetContext.Jobs.Where(x => (x.Status == JobStatus.Started || x.Status == JobStatus.BeingReturned) && x.PickCompleted.HasValue).ToList().GroupBy(x => x.PickCompleted.Value.Date).OrderBy(x => x.Key);
             base.OnActionExecuting(filterContext);
         }
     }
