2e77972a2b4155fa694090cfbb9dca15ea2ba104
Nick Nystrom <nick.nystrom@belaysoftware.com>
2013-01-16 15:19:36 -0600

    Basic maxwell client, no connectivity to website yet.

---
+++ MaxwellSync/MaxwellConnection.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.Odbc;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace MaxwellSync
+{
+    public class MaxwellCustomer
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+    }
+
+    /// <summary>
+    /// The MaxwellManager is used to import data from Maxwell into the AssetTracker system.
+    /// </summary>
+    public class MaxwellConnection : IDisposable
+    {
+        private readonly OdbcConnection _cx;
+
+        public MaxwellConnection()
+            : this(null)
+        {
+
+        }
+
+        public MaxwellConnection(string maxwellConnectionString)
+        {
+            // AssetMaxwellInventoryItem: CINVM (Inventory Item Master)
+            // AssetMaxwellJobHeader: CWIPH
+            //  ""            Detail: CINRQ
+            // Projet Manager File:   CSAMS
+            // Job Class File:        CSCSY08 Description of codes related to jobs
+
+            _cx = new OdbcConnection(
+                maxwellConnectionString ??
+                ConfigurationManager.AppSettings["MaxwellConnectionString"]
+            );
+            _cx.Open();
+        }
+
+        /*
+
+        public string ImportInventory()
+        {
+            var updated = 0;
+            var inserted = 0;
+            using (var db = new AssetDBDataContext())
+            {
+                var odbc = new OdbcCommand("select * from CINVM", _cx);
+                var dr = odbc.ExecuteReader();
+                while (dr.Read())
+                {
+                    var co_code = dr["CO_CODE"] as string;
+                    var inv_item_no = dr["INV_ITEM_NO"] as string;
+                    var i = db.AssetMaxwellInventoryItems.FirstOrDefault(
+                        x => x.CO_CODE == co_code &&
+                             x.INV_ITEM_NO == inv_item_no);
+                    if (null == i)
+                    {
+                        i = new AssetMaxwellInventoryItem
+                        {
+                            CO_CODE = co_code,
+                            INV_ITEM_NO = inv_item_no,
+                        };
+                        db.AssetMaxwellInventoryItems.InsertOnSubmit(i);
+                        inserted++;
+                    }
+
+                    i.INV_ITEM_DESC = dr["INV_ITEM_DESC"] as string;
+                    i.ALT_ITEM_NO = dr["ALT_ITEM_NO"] as string;
+                    i.NOT_USED_A = dr["NOT_USED_A"] as string;
+                    i.NOT_USED_B = dr["NOT_USED_B"] as string;
+                    i.UNIT_OF_MEASURE = dr["UNIT_OF_MEASURE"] as string;
+                    i.TAXABLE_YN = dr["TAXABLE_YN"] as string;
+                    i.UNIT_LIST_PRICE = dr["UNIT_LIST_PRICE"] as double?;
+                    i.UNIT_COST = dr["UNIT_COST"] as double?;
+                    i.ALT_COMM_BASIS = dr["ALT_COMM_BASIS"] as double?;
+                    i.REORDER_PNT_TOT = dr["REORDER_PNT_TOT"] as double?;
+                    i.LEAD_TIME = dr["LEAD_TIME"] as double?;
+                    i.QTY_ON_HAND_TOT = dr["QTY_ON_HAND_TOT"] as double?;
+                    i.QTY_COMMITD_TOT = dr["QTY_COMMITD_TOT"] as double?;
+                    i.QTY_ON_ORDR_TOT = dr["QTY_ON_ORDR_TOT"] as double?;
+                    i.QTY_SLD_YTD_TOT = dr["QTY_SLD_YTD_TOT"] as double?;
+                    i.ITEM_CATEGORY = dr["ITEM_CATEGORY"] as string;
+                    i.COST_METHOD = dr["COST_METHOD"] as string;
+                    i.SALES_COMM_PCT = dr["SALES_COMM_PCT"] as double?;
+                    i.COMMISSIONABLE = dr["COMMISSIONABLE"] as string;
+                    i.EST_UNIT_COST = dr["EST_UNIT_COST"] as double?;
+                    i.EST_MAN_HR_UNIT = dr["EST_MAN_HR_UNIT"] as double?;
+                    i.EST_LABOR_CLASS = dr["EST_LABOR_CLASS"] as string;
+                    i.UPC_NO = dr["UPC_NO"] as string;
+                    i.EST_LABOR_RATE = dr["EST_LABOR_RATE"] as double?;
+                    i.EST_MISC_1 = dr["EST_MISC_1"] as double?;
+                    i.EST_MISC_2 = dr["EST_MISC_2"] as double?;
+                    i.EST_MISC_3 = dr["EST_MISC_3"] as double?;
+                    i.BILLING_CTGY = dr["BILLING_CTGY"] as string;
+
+                    updated++;
+                }
+
+                db.SubmitChanges();
+            }
+            return String.Format("{0} inventory items modified ({1} new records)", updated, inserted);
+        }
+
+        */
+
+        public IList<MaxwellCustomer> ImportCustomers()
+        {
+            var customers = new List<MaxwellCustomer>();
+            using (var cmd = new OdbcCommand("select * from CCSMS", _cx))
+            {
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        customers.Add(new MaxwellCustomer
+                        {
+                            Number = dr["CUST_NO"] as string,
+                            Name = dr["CUST_NAME"] as string
+                        });
+                    }
+                }
+            }
+            return customers;
+        }
+
+        public void Dispose()
+        {
+            if (_cx.State == ConnectionState.Open)
+            {
+                _cx.Dispose();
+            }
+        }
+    }
+}
---
+++ MaxwellSync/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxwellSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cx = new MaxwellConnection();
+            foreach (var c in cx.ImportCustomers())
+            {
+                Console.WriteLine("Customer: {0} (#{1})", c.Name, c.Number);
+            }
+        }
+    }
+}
