4d6e8ae678c5800a04d201954e8ce380e65ef9b5
Nick Nystrom <nick.nystrom@belaysoftware.com>
2013-04-22 16:58:26 -0500

    Resolves #156

--- Mavo.Asset/Controllers/AssetController.cs
+++ Mavo.Asset/Controllers/AssetController.cs
@@ -216,9 +216,11 @@ namespace Mavo.Assets.Controllers
             if (search.CategoryId.HasValue)
                 query = query.Where(x => x.Category.Id == search.CategoryId);
             if (!String.IsNullOrEmpty(search.SearchString))
-                query = query.Where(x => x.Items.Any(i => i.Barcode.Contains(search.SearchString))
+                query = query.Where(x => x.Items.Any(i => 
+                       i.Barcode.Contains(search.SearchString))
                     || x.Name.Contains(search.SearchString)
                     || x.Category.Name.Contains(search.SearchString)
+                    || x.MavoItemNumber.Contains(search.SearchString)
                     );
             search.Results = query.Select(x => new AssetSearchResult()
             {
