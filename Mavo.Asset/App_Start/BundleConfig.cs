0c233e027a714e4da2b817cf2f866c8d68b5e18a
Nick Nystrom <nnystrom@gmail.com>
2014-12-16 13:01:25 -0600

    Barcodes scan everywhere

--- Mavo.Asset/App_Start/BundleConfig.cs
+++ Mavo.Asset/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace Mavo.Assets
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                   "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/barcodes").Include("~/Scripts/barcodes.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ Mavo.Asset/Scripts/barcodes.js
@@ -0,0 +1,67 @@
+﻿// ---------------------------------------- barcode scanning recognition
+
+// receive barcodes using:
+//   $(window).on('barcode', function (e, barcode) { });
+
+var control = ['{', '}'];
+var buf = null;
+var timer = null;
+var timeout = function () {
+    buf = null;
+    timer = null;
+};
+$(window).keypress(function (e) {
+    var handled = false;
+    var key = String.fromCharCode(e.charCode);
+    if (key === control[0]) {
+
+        // start marker
+        buf = [];
+        handled = true;
+
+        // reset timeout
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(timeout, 1000);
+    }
+    else if (key === control[1]) {
+
+        // end marker
+        var barcode = buf.join('');
+        buf = null;
+        handled = true;
+
+        // clear existing timeout
+        if (timer) clearTimeout(timer);
+        timer = null;
+
+        if (barcode) {
+
+            // try to send to a receiver textbox if one is focused
+            var receiver = $(':input[data-barcode-receiver]');
+            if (receiver.length > 0) {
+                receiver.val(barcode);
+                if (receiver.is('[data-barcode-submit]')) {
+                    receiver.closest('form').submit();
+                }
+            }
+            else {
+                $(window).trigger('barcode', barcode);
+            }
+        }
+    }
+    else if (buf !== null) {
+
+        // barcode character
+        handled = true;
+        buf.push(key.trim())
+
+        // reset timeout
+        if (timer) clearTimeout(timer);
+        timer = setTimeout(timeout, 1000);
+    }
+    if (handled) {
+        e.preventDefault();
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+    }
+});
